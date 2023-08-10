85c19bdea2a8b9e191409e4fb347ff8b2b27ccd5
mennaramadan9 <mennaramadan56@gmail.com>
2023-08-10 09:53:04 -0700

    update puts betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - main function
- * Return 0
+ *
+ * Return : always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

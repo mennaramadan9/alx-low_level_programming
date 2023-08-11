ff724a89ee647287c0077ee1d7819363f894817e
mennaramadan9 <mennaramadan56@gmail.com>
2023-08-11 15:09:46 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C prograam to print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		printf("%c", i);
+	}
+	printf("\n");
+	return (0);
+}

2c20ff9639fdbaaad28b19494215f21b709f8d49
mennaramadan9 <mennaramadan56@gmail.com>
2023-08-15 16:52:10 -0700

    edit i

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,11 +15,11 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (n != 98)
+			if (i != 98)
 			{
 				printf("%d, ", i);
 			}
-			else if (n == 98)
+			else if (i == 98)
 			{
 				printf("%d\n", i);
 			}
@@ -29,11 +29,11 @@ void print_to_98(int n)
 	{
 		for (j = n; j >= 98; j--)
 		{
-			if (n != 98)
+			if (j != 98)
 			{
 				printf("%d, ", j);
 			}
-			else if (n == 98)
+			else if (j == 98)
 			{
 				printf("%d\n", j);
 			}

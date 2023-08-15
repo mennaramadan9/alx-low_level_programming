ab86143e1dd19607eba7bacd6a243a1565740c9d
mennaramadan9 <mennaramadan56@gmail.com>
2023-08-15 15:20:34 -0700

    edit on putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include "main.h"
-
 /**
  * main - print _putchar
- * is written by mbah
+ * is writtn by mbah
  * Return: 0
  */
+
+
 int main(void)
 {
 	_putchar('_');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef main_h
 #define main_h
 
-int _putchar(char)
+
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +14,8 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
-#endif;
+
+#endif
+

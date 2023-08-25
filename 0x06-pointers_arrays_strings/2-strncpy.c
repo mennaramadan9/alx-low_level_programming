ba820f4cc88f3a2e3aaf6be1323e9cf60a2013c7
mennaramadan9 <mennaramadan56@gmail.com>
2023-08-25 05:18:01 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: string
+ * @src: string
+ * @n: variable
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *originalDest = dest;
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+	return (originalDest);
+}

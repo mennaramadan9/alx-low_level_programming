9a59f604dee6f5d2cbc7eb3535b00d2d39708445
mennaramadan9 <mennaramadan56@gmail.com>
2023-08-31 13:29:09 -0700

    sqrt recuirsive

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,13 +3,29 @@
 /**
  * _sqrt_recursion - function that returns the natural square root of a number
  * @n: variable
+ * @guess: variable
  * Return: result
  */
+int _sqrt_recursive(int n, int guess);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 1 || n == 0)
+	if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt_recursion(n / 2) + 1);
+	return (_sqrt_recursive(n, 1));
+}
+/**
+ * _sqrt_recursive - find sqrt
+ * @n: number
+ * @guess: variable
+ * Return: variable
+ */
+int _sqrt_recursive(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+	else if (guess * guess > n)
+		return (-1);
+	return (_sqrt_recursive(n, guess + 1));
 }

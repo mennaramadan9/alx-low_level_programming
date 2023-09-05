f89bcb6aa10110be1cbff25641daf95979af4c80
mennaramadan9 <mennaramadan56@gmail.com>
2023-09-05 01:23:59 -0700

    edit of memory alocation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = (int **)malloc(height * sizeof(int));
+	array = (int **)malloc(height * sizeof(int *));
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)

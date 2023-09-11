b5a6b04d4cfc97e0f1d90f8c29e904df9ba06358
mennaramadan9 <mennaramadan56@gmail.com>
2023-09-11 06:30:23 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: string
+ * @age: float
+ * @owner: string
+ *
+ * Description: Test
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

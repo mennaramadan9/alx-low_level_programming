8670a669d69bcaf5acfa6624e98f4c783c46a837
mennaramadan9 <mennaramadan56@gmail.com>
2023-09-21 15:17:45 -0700

    i

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list
+ * @head: A pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr->str);
+		free(ptr);
+	}
+}
